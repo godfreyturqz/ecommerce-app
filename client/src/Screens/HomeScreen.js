c25aba7f3e94ee81e55ebd44b7153595b055dde7
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-10-31 20:27:52 +0800

    add redux

--- client/src/Screens/HomeScreen.js
+++ client/src/Screens/HomeScreen.js
@@ -2,23 +2,44 @@ import React, { useState, useEffect } from 'react'
 import { Link } from "react-router-dom";
 // import data from "./../Data";
 import './../App.css';
-import axios from "axios";
+// import axios from "axios";
+import { useSelector, useDispatch } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 
 function HomeScreen(props) {
-    const [products, setProducts] = useState([])   
+    // const [products, setProducts] = useState([])
+    const productList = useSelector(state => state.productList)
+    const { products, loading, error } = productList
+    const dispatch = useDispatch()
 
+    // useEffect(() => {
+    //     const fetchData = async ()=> {
+    //         const {data} = await axios.get('/api/products')
+    //         setProducts(data)
+    //     }
+    //     fetchData()
+    //     return () => {
+    //         //
+    //     }
+    // }, [])
     useEffect(() => {
-        const fetchData = async ()=> {
-            const {data} = await axios.get('/api/products')
-            setProducts(data)
-        }
-        fetchData()
+        dispatch(listProducts())
         return () => {
             //
         }
     }, [])
-    
-    return (
+
+    // useEffect(()=> {
+    //     axios.get(`/api/products`)
+    //     .then(res => {
+    //         setProducts(res.data)
+    //     })
+    //     .catch(err => {
+    //         console.log(err)
+    //     })
+    // }, [])
+
+return ( loading ? <div>Loading...</div> : error ? <div>{error}</div> :
         <ul className="products">
             {/* { data.products.map(product => */}
             { products.map(product =>
---
+++ client/src/store.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers } from "redux";
+import { productListReducer } from "./reducers/productReducers";
+
+
+const initialState= {}
+const reducer = combineReducers({
+    productList: productListReducer
+})
+
+
+const store = createStore(reducer, initialState)
+export default store
