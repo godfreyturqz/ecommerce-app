ebc138cfc44357538c66663ff8be7192756eb918
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-11-06 03:34:52 +0800

    setup prior to deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 require('dotenv/config')
+const path = require('path')
 
 const app = express()
 
@@ -12,7 +13,12 @@ app.use(cors())
 //routes
 app.use('/api', require('./routes/api'))
 
-
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res)=> {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.DB_CONNECTION, {
