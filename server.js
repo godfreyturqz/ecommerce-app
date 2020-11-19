1d6062ce15963399a2b48d104f86b2c80f447979
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-11-19 12:03:51 +0800

    added compression pkg
    for faster app loading

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const compression = require('compression')
 const mongoose = require('mongoose')
 const cors = require('cors')
 require('dotenv/config')
@@ -9,6 +10,7 @@ const app = express()
 app.use(express.json({limit: "30mb", extended: true}))
 app.use(express.urlencoded({limit: "30mb", extended: true}))
 app.use(cors())
+app.use(compression())
 
 //routes
 app.use('/api', require('./routes/api'))
