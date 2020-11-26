90062c2c3b506d1b928294387b7aa18a9bfb8c81
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-11-26 15:11:11 +0800

    minor changes

--- client/src/redux/user/userReducers.js
+++ client/src/redux/user/userReducers.js
@@ -1,9 +1,4 @@
-const userSignInState= {
-    loading: false,
-    userInfo: {},
-    error: ''
-}
-export function userSignInReducer(state= userSignInState, action){
+export function userSignInReducer(state= { loading: false, userInfo: {}, error: '' }, action){
     switch(action.type){
         case 'USER_SIGNIN_REQUEST':
             return {
