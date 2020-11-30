d9c95f58e4245d5347ce754b9977fcbc04bb36d8
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-11-30 15:17:20 +0800

    intial logout function

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,14 +2,22 @@ import React from 'react'
 import './styles.css'
 import { FaBars, FaCartArrowDown } from 'react-icons/fa';
 import { Link } from "react-router-dom";
+import axios from 'axios';
 //redux
 import { useSelector } from "react-redux";
 
 
 function Navbar() {
     const cartItems = useSelector(state => state.cartReducer)
+
     function openMenu(){
       document.querySelector('.sidebar').classList.add('open')
+    }
+
+    const handleLogout = ()=>{
+      axios.get('/api/logout')
+      // .then((data) => console.log(data))
+      // .catch(error => console.log(error))
     }
 
     return (
@@ -23,6 +31,8 @@ function Navbar() {
               {cartItems.data.length > 0 ? <span>{cartItems.data.length}</span> : null}
               <FaCartArrowDown /> Cart
             </Link>
+            <Link to='/account'>Account</Link>
+            <Link to="/" onClick={handleLogout}>Logout</Link>
             <Link to='/signin'>Sign-in</Link>
             <Link to='/register'>Register</Link>
           </div>
