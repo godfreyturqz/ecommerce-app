c3784f8d2c4c24882d52c95faa79f3fcb03f88f3
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-12-02 17:51:21 +0800

    added addtl details

--- models/OrderModel.js
+++ models/OrderModel.js
@@ -2,12 +2,16 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const orderSchema = new Schema({
-    // userId : {type: Number, required: true},
+    userId : {type: String, required: true},
     orderItems: [{
         productId: {type: String, required: true},
+        mainCategory: {type: String, required: true},
+        subCategory: {type: String, required: true},
+        brand: {type: String, required: true},
         name: {type: String, required: true},
-        quantity: {type: Number, required: true},
-        price: {type: Number, required: true}
+        image: {type: String, required: true},
+        price: {type: Number, required: true},
+        quantity: {type: Number, required: true}
     }],
     shippingData: {
         fullName: {type: String, required: true},
@@ -29,6 +33,10 @@ const orderSchema = new Schema({
         type: String,
         default: 'On process'
     },
+    orderDate : {
+        type: Date,
+        default: Date.now
+    }
 })
 
 const OrderModel = mongoose.model('orders', orderSchema)
