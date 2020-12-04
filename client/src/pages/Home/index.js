5b213ac0e26a4077f1bf64ac26ab117906ec818a
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-12-04 23:20:54 +0800

    separate logic and ui of homepage

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,48 +1,36 @@
-//styles
-import './styles.css';
-//react
-import React, { useEffect } from 'react'
-//redux and action creators
-import { useSelector, useDispatch } from 'react-redux';
-import { getProducts } from '../../redux/product/productActions';
+import React from 'react'
+import './styles.css'
+import HomeLogic from './HomeLogic'
 //components
 import Loading from '../../components/Loading';
 import ProductCard from '../../components/ProductCard';
 
 
 function Home() {
-    const {loading, data, error} = useSelector(state => state.getProductsReducer)
-    const dispatch = useDispatch()
-    
-    useEffect(() => {
-
-        dispatch(getProducts())
-
-    }, [dispatch])
+    const { getProductsReducer } = HomeLogic()
     
     return ( 
-        loading ? <Loading/> : 
-        error ? <div>{error}</div> :
-        data && data.length !== 0 ?
+        getProductsReducer.loading ? <Loading/> : 
+        getProductsReducer.error ? <div>{getProductsReducer.error}</div> :
+        getProductsReducer.data && getProductsReducer.data.length !== 0 ?
         <div className="products-container">
             {
-                data.map( product =>
-
-                <ProductCard
-                key={product._id}
-                to={'/product/details/' + product._id}
-                image={product.image}
-                name={product.name}
-                mainCategory={product.mainCategory}
-                subCategory={product.subCategory}
-                brand={product.brand}
-                price={product.price}
-                />
-
+                getProductsReducer.data.map( product =>
+                    <ProductCard
+                    key={product._id}
+                    to={`/product/details/${product._id}`}
+                    image={product.image}
+                    name={product.name}
+                    mainCategory={product.mainCategory}
+                    subCategory={product.subCategory}
+                    brand={product.brand}
+                    price={product.price}
+                    />
                 )
             }
         </div>
-        : <div>There are no products to display.</div>
+        :
+        <div>There are no products to display.</div>
     )
 }
 
--- client/src/pages/OrderStatus/index.js
+++ client/src/pages/OrderStatus/index.js
@@ -3,14 +3,25 @@ import { useSelector, useDispatch } from 'react-redux'
 import { getOrders } from '../../redux/order/orderActions'
 import OrderRowCard from "../../components/OrderRowCard";
 import './styles.css'
+import axios from 'axios';
 
 
-function OrderStatus() {
+function OrderStatus(props) {
     const getOrdersReducer = useSelector(state => state.getOrdersReducer)
+
+    const query = new URLSearchParams(props.location.search)
+    console.log(query)
+
 
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(getOrders())
+        if(query.get('paymentId') && query.get('PayerID')){
+            axios.post('/api/payment/paypalExecute',{
+                paymentId: query.get('paymentId'),
+                PayerID: query.get('PayerID')
+            })
+        }
 
     }, [dispatch])
     
