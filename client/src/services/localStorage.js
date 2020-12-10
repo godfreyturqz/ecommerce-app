18eb9ad4fb769fb1eb71612e52b29e8cb20fbc5b
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-12-10 18:57:22 +0800

    added localstorage service
    other changes: rename function name of checkLocalStorageExpiration

--- client/src/services/localStorage.js
+++ client/src/services/localStorage.js
@@ -1,4 +1,4 @@
-export const checkLocalStorageExpiration = (key) => {
+export const isStorageExpired = (key) => {
 	const itemStr = localStorage.getItem(key)
 	if (!itemStr) return null
 
@@ -10,5 +10,22 @@ export const checkLocalStorageExpiration = (key) => {
 		localStorage.removeItem(key)
 		return null
 	}
+
 	return item.value
+}
+
+export const setProductStorage = (data) => {
+	const item = {
+		value: data, 
+		maxAge : new Date().getTime() + 86400000
+	}
+
+	return localStorage.setItem("products", JSON.stringify(item))
+}
+
+export const getProductStorage = (productId) => {
+	const itemStr = localStorage.getItem("products")
+	if (!itemStr) return null
+
+	return JSON.parse(itemStr).value.find(product => product._id === productId)
 }
