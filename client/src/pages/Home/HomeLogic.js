030eee994df60c363bb33a20678c462cbe5a8657
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-12-11 10:25:06 +0800

    improved filter function

--- client/src/pages/Home/HomeLogic.js
+++ client/src/pages/Home/HomeLogic.js
@@ -1,18 +1,32 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
 import { getProducts } from '../../redux/product/productActions';
 
 function HomeLogic() {
+
     const getProductsReducer = useSelector(state => state.getProductsReducer)
+    const [sortByPrice, setSortByPrice] = useState(1)
     const dispatch = useDispatch()
     
     useEffect(() => {
         dispatch(getProducts())
 
     }, [dispatch])
+
+    const handleSortByPrice = (e) => {
+        if(e.target.value === 'highest') {
+            setSortByPrice(-1)
+        } else {
+            setSortByPrice(1)
+        }
+    }
     
 
-    return { getProductsReducer }
+    return { 
+        getProductsReducer,
+        sortByPrice,
+        handleSortByPrice
+    }
 }
 
 export default HomeLogic
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './styles.css'
 import HomeLogic from './HomeLogic'
 //components
@@ -7,35 +7,46 @@ import ProductCard from './ProductCard';
 
 
 function Home({filteredProducts}) {
-    const { getProductsReducer } = HomeLogic()
-    const [sortByPrice, setSortByPrice] = useState(1)
+
+    const { 
+        getProductsReducer,
+        sortByPrice,
+        handleSortByPrice
+    } = HomeLogic()
+    
     
     return ( 
         getProductsReducer.loading ? <Loading/> : 
         getProductsReducer.error ? <div>{getProductsReducer.error}</div> :
-        getProductsReducer.data && getProductsReducer.data.length !== 0 ?
-        <div className="products-container">
-            <button onClick={()=> setSortByPrice(prev => prev * -1)}>Sort by price</button>
-            {
-                getProductsReducer.data
-                .sort((a, b)=> a.price > b.price ? sortByPrice : sortByPrice * -1)
-                .filter(product => filteredProducts ? product.mainCategory === filteredProducts : product)
-                .map( product =>
-                    <ProductCard
-                    key={product._id}
-                    to={`/product/details/${product._id}`}
-                    image={product.image}
-                    name={product.name}
-                    mainCategory={product.mainCategory}
-                    subCategory={product.subCategory}
-                    brand={product.brand}
-                    price={product.price}
-                    />
-                )
-            }
-        </div>
-        :
-        <div>There are no products to display.</div>
+        getProductsReducer.data.length === 0 ? <div>There are no products to display.</div> :
+        <>
+            <div className="filter-container">
+                <span>Sort by price: </span>
+                <select onChange={handleSortByPrice}>
+                    <option value="lowest">Lowest</option>
+                    <option value="highest">Highest</option>
+                </select>
+            </div>
+            <div className="products-container">
+                {
+                    getProductsReducer.data
+                    .sort((prev, next)=> prev.price > next.price ? sortByPrice : sortByPrice * -1)
+                    .filter(product => filteredProducts ? product.mainCategory === filteredProducts : product)
+                    .map( product =>
+                        <ProductCard
+                        key={product._id}
+                        to={`/product/details/${product._id}`}
+                        image={product.image}
+                        name={product.name}
+                        mainCategory={product.mainCategory}
+                        subCategory={product.subCategory}
+                        brand={product.brand}
+                        price={product.price}
+                        />
+                    )
+                }
+            </div>
+        </>
     )
 }
 
