076d9fd312a8766a9fa86718aba43e951ad8586b
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-12-13 16:37:54 +0800

    removed try catch statement

--- client/src/redux/cart/cartActions.js
+++ client/src/redux/cart/cartActions.js
@@ -1,35 +1,28 @@
-// import axios from "axios";
+import { LocalStorage } from '../../services/LocalStorage'
 
+const localStr = new LocalStorage()
 
-export const addToCart = (productId, quantity) => async (dispatch) => {
+export const addToCart = (productId, quantity) => {
 
-    try {
-        const data = JSON.parse(localStorage.getItem('getProducts')).find(product => product._id === productId)
-        // const {data} = await axios.get(`/api/products/${productId}`)
-        // localStorage.setItem('cart', JSON.stringify(data))
-        dispatch({
-            type: 'ADD_TO_CART_SUCCESS',
-            payload: {
-                productId: data._id,
-                mainCategory: data.mainCategory,
-                subCategory: data.subCategory,
-                brand: data.brand,
-                name: data.name,
-                image: data.image,
-                price: data.price,
-                quantity: quantity
-            }
-        })
-    }
-    catch (error) {
-        dispatch({type: 'ADD_TO_CART_FAIL', payload: error.message})
+    const data = localStr.getProductStorage(productId)
+    if (data === null) return
+    
+
+    return {
+        type: 'ADD_TO_CART_SUCCESS',
+        payload: {
+            productId: data._id,
+            mainCategory: data.mainCategory,
+            subCategory: data.subCategory,
+            brand: data.brand,
+            name: data.name,
+            image: data.image,
+            price: data.price,
+            quantity: quantity
+        }
     }
 }
 
-
-export const removeFromCart = (productId) => (dispatch) => {
-    // const data = JSON.parse(localStorage.getItem('cart')).filter(product => product._id !== productId)
-    // localStorage.setItem('cart', JSON.stringify(data))
-    // localStorage.removeItem('cart', productId)
-    dispatch({type: 'REMOVE_FROM_CART', payload: productId})
+export const removeFromCart = (productId) => {
+    return {type: 'REMOVE_FROM_CART', payload: productId}
 }
