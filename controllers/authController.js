a7915ae57b9c4def1eb7de2ac38c003afe22f04c
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2020-12-17 15:47:02 +0800

    transfer login error handling to utils.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,17 +3,20 @@ const utils = require('../utils')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
+
 //------------------------------------
 // SIGNUP
 //------------------------------------
 module.exports.createUser = async (req, res)=> {
     try {
+
         const userData = await UserModel.create(req.body)
         const token = utils.createToken(userData._id)
         res.cookie('jwt', token, { secure: true, httpOnly: true, maxAge: 259200000})
         res.status(201).json({userId: userData._id})
 
     } catch (error) {
+
         const errors = utils.handleErrors(error)
         res.status(400).json({message: 'user not created', errors})
     }
@@ -31,26 +34,33 @@ module.exports.getUsers = async (req,res) => {
 // LOGIN
 //------------------------------------
 module.exports.loginUser = async (req,res) => {
-    if(!req.body.email) return res.status(400).json({email: 'Enter an Email'})
-    if(!req.body.password) return res.status(400).json({password: 'Enter a password'})
+
+    const email = req.body.email
+    const password = req.body.password
 
     try {
-        const userData = await UserModel.findOne({email: req.body.email})
-        if(userData === null) return res.status(400).json({email: 'Account does not exists'})
+        
+        if(!email && !password) throw Error('empty field')
+        if(!email) throw Error('enter email')
+        if(!password) throw Error('enter password')
 
-        const isAuth = await bcrypt.compare(req.body.password, userData.password)
+        const userData = await UserModel.findOne({email})
+        if(userData === null) throw Error('not registered')
+
+        const isAuth = await bcrypt.compare(password, userData.password)
+
         if(isAuth) {
+
             const token = utils.createToken(userData._id)
-            res.cookie('jwt', token, { secure: true, httpOnly:true, maxAge: 3 * 24 * 60 * 60 * 1000})
-            res.status(200).json({userId: userData._id}) 
-        }
-        else {
-            res.status(400).json({password: 'Some of your information isn\'t correct. Please try again.'})
-        }
+            res.cookie('jwt', token, { secure: true, httpOnly:true, maxAge: 86400000 })
+            res.status(200).json({userId: userData._id})
+
+        } else throw Error('authentication failed')
 
     } catch (error) {
-        console.log(error)
-        res.status(400).json({message: 'login failed', error})
+
+        const errors = utils.handleErrors(error)
+        res.status(400).json({message: 'login failed', errors})
     }
 }
 
@@ -58,7 +68,7 @@ module.exports.loginUser = async (req,res) => {
 // LOGOUT
 //------------------------------------
 module.exports.logout = (req, res) => {
-    res.cookie('jwt', '', { secure: true, httpOnly:true, maxAge: 1}).send()
+    res.cookie('jwt', '', { secure: true, httpOnly:true, maxAge: 1 }).send()
 }
 
 // for dev purposes
@@ -75,13 +85,13 @@ module.exports.isAuth = (req,res)=>{
     const token = req.cookies.jwt
 
     if(token){
+
         jwt.verify(token, process.env.JWT, (error, decodedToken) => {
             if (error) return res.sendStatus(403)
             // decodedToken returns userId
             res.status(200).json(decodedToken)
         })
-    }
-    else {
-        res.status(401).json({message: 'Not Authenticated'})
-    }
+
+    } else res.status(401).json({message: 'Not Authenticated'})
+    
 }
